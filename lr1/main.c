f4847610fa9ff46b4821559a5fa6c21189fb3591
Andrey <shevchenko.andrey.uzurpastor@gmail.com>
2023-11-20 12:52:29 +0200

    jumpshot -> set up

--- lr1/main.c
+++ lr1/main.c
@@ -1,42 +1,72 @@
+#include <mpi.h>
+#include "mpe.h"
 #include "employee_filtering.h"
 #include "localmpi.h"
-#include "mpi.h"
 
 
 int main()
 {
-	MPI_Init(NULL, NULL);
-	initializeCurrentYear();
-
 	int world_size, world_rank;
 	int total_size, chunk_size, chunk_fsize, total_fsize;
 	Employee *total_list = NULL, 
 		 *chunk_list = NULL,
 		 *chunk_flist = NULL, 
 		 *total_flist = NULL;
+	int ev1a, ev1b, ev2a, ev2b, ev3a, ev3b, ev4a, ev4b;
 
+	MPI_Init(NULL, NULL);
+	initializeCurrentYear();
 	initializeSizeAndRank(&world_size, &world_rank);
 
+	MPE_Init_log();
+	ev1a = MPE_Log_get_event_number();
+        ev1b = MPE_Log_get_event_number();
+        ev2a = MPE_Log_get_event_number();
+        ev2b = MPE_Log_get_event_number();
+        ev3a = MPE_Log_get_event_number();
+        ev3b = MPE_Log_get_event_number();
+        ev4a = MPE_Log_get_event_number();
+        ev4b = MPE_Log_get_event_number();
+	if(world_rank == 0)
+	{
+                MPE_Describe_state(ev1a, ev1b, "Broadcast", "red");
+                MPE_Describe_state(ev2a, ev2b, "Compute", "blue");
+                MPE_Describe_state(ev3a, ev3b, "Reduce", "green");
+                MPE_Describe_state(ev4a, ev4b, "Sync", "orange");
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	MPE_Log_event(ev1a, 0, "start broadcast");
 	mainInitializeEmployees(world_rank, "employees.csv", 
 			&total_list, &total_size);
 	bcastSize(&total_size);
 
 	mainSendChunks(world_rank, world_size, total_list, total_size);
 
-	MPI_Barrier(MPI_COMM_WORLD);
 	otherProcessesRecvChunk(world_rank, world_size, total_list,
 			   	total_size, &chunk_list, &chunk_size);
+	MPE_Log_event(ev1b, 0, "end broadcast");
 
+	MPE_Log_event(ev4a, 0, "start sync");
 	MPI_Barrier(MPI_COMM_WORLD);
+	MPE_Log_event(ev4b, 0, "end sync");
+
+	MPE_Log_event(ev2a, 0, "start compute");
 	filterEmployees(chunk_list, chunk_size, &chunk_flist, 
 			&chunk_fsize);
+	MPE_Log_event(ev2b, 0, "end compute");
 
+	MPE_Log_event(ev4a, 0, "start sync");
 	MPI_Barrier(MPI_COMM_WORLD);
-	summirizeFilteredSize(chunk_fsize, &total_fsize);
+	MPE_Log_event(ev4b, 0, "end sync");
+
 	
-	MPI_Barrier(MPI_COMM_WORLD);
+	MPE_Log_event(ev3a, 0, "start reduce");
+	summirizeFilteredSize(chunk_fsize, &total_fsize);
 	collectFilteredLists(world_rank, world_size, &total_flist,
 			   	total_fsize, chunk_flist, chunk_fsize);
+	MPE_Log_event(ev3b, 0, "end reduce");
 
 	mainPrintEmployees(world_rank, total_flist, total_fsize);
 
@@ -50,6 +80,7 @@ int main()
 
 	free(currentYearPtr);
 
+	MPE_Finish_log("./logs/graphlog");
 	MPI_Finalize();
 	return 0;
 }
